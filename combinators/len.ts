fefbdccb6c16ff621ef233c56946dded5006acb5
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2022-12-01 23:46:32 +0100

    remove len2

--- combinators/len.ts
+++ combinators/len.ts
@@ -2,22 +2,6 @@ import { Parser } from '~types/parser.ts';
 import { Tuple } from '~types/util.ts';
 import { oneOrMore } from './one_or_more.ts';
 
-export const len2 = <T, N extends number>(
-  parser: Parser<T>,
-  length: N
-): Parser<Tuple<T, N>> =>
-  (() => {
-    return oneOrMore(parser).chain(({ result, ...rest }) => {
-      if (result.length === length)
-        return { result: result as Tuple<T, N>, ...rest };
-      else
-        return {
-          error: `expected ${length} occurrences of ${parser.expects}`,
-          ...rest,
-        };
-    });
-  })();
-
 export function len<T, N extends number>(parser: Parser<T>, length: N) {
   return oneOrMore(parser).chain(({ result, ...rest }) => {
     if (result.length === length)
